84131d1dab588140c8d66ef7bb4efc2e40269000
Daniel Calle <nieled@riseup.net>
2021-08-26 23:14:51 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const APP_DIR = path.resolve(__dirname, './src');
+const PUBLIC_DIR = path.resolve(__dirname, './public');
+const BUILD_DIR = path.resolve(__dirname, './dist');
+
+const isDevMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  mode: isDevMode ? 'development' : 'production',
+  entry: {
+    app: APP_DIR + "/index.tsx"
+  },
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif|ttf|json)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  devServer: {
+    static: {
+      directory: BUILD_DIR
+    },
+    compress: true,
+    port: 9000,
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: PUBLIC_DIR + "/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevMode ? '[id].css' : '[id].[hash].css'
+    }),
+  ],
+}
