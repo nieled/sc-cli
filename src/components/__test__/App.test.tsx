6b41b758f0deb75e934800e3f1b39ed11820e680
Daniel Calle <nieled@riseup.net>
2021-08-26 23:45:10 -0500

    Add tests and prettier

---
+++ src/components/__test__/App.test.tsx
@@ -0,0 +1,12 @@
+import { expect, test } from "@jest/globals";
+
+describe('App component', () => {
+
+  test('true is truthy', () => {
+    expect(true).toBe(true);
+  });
+
+  test('false is falsy', () => {
+    expect(false).toBe(false);
+  });
+})
