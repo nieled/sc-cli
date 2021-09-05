93c5c9531b23a047eeeddaa2f0632644cf1e925d
Daniel Calle <nieled@riseup.net>
2021-09-05 09:58:09 -0500

    Include subRoutes on app menu

--- src/components/AppMenu.tsx
+++ src/components/AppMenu.tsx
@@ -1,12 +1,22 @@
+import React from 'react';
 import {
+	Collapse,
 	createStyles,
+	Icon,
+	IconButton,
 	List,
 	ListItem,
+	ListItemIcon,
+	ListItemText,
 	makeStyles,
 	Theme,
+	Tooltip,
 } from '@material-ui/core';
-import React from 'react';
+import clsx from 'clsx';
 import { useLocation } from 'react-router-dom';
+import DefaultIcon from '@material-ui/icons/FileCopy';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 import { routes } from '../config/routes';
 import RouteItem from '../models/RouteItem.model';
 import MenuItem from './MenuItem';
@@ -16,6 +26,14 @@ const useStyles = makeStyles((theme: Theme) =>
 		divider: {
 			marginTop: theme.spacing(1),
 			marginBottom: theme.spacing(1),
+		},
+		nested: {
+			marginLeft: theme.spacing(2),
+		},
+		selected: {
+			transition: 'box-shadow',
+			transitionDuration: '1s',
+			boxShadow: `0 0 3px ${theme.palette.primary.main}, 0 0 9px ${theme.palette.primary.main}, 0 0 11px ${theme.palette.primary.main}, 0 0 30px ${theme.palette.primary.main}`,
 		},
 	})
 );
@@ -29,12 +47,44 @@ const AppMenu = () => {
 		setOpen(!open);
 	};
 
-	// TODO: Add subMenus
 	return (
 		<List>
 			{routes.map((route: RouteItem) => (
 				<>
-					<MenuItem key={route.key} route={route} />
+					{route.subRoutes ? (
+						<ListItem button onClick={handleClick}>
+							<ListItemIcon>
+								<IconButton
+									className={clsx({
+										[classes.selected]:
+											!open &&
+											route.subRoutes.some(
+												(item: RouteItem) => item.path === location.pathname
+											),
+									})}
+									size="small"
+								>
+									<Icon component={route.icon || DefaultIcon} />
+								</IconButton>
+							</ListItemIcon>
+							<ListItemText primary={route.title} />
+							<Tooltip
+								title={`${open ? 'Collapse' : 'Expand'}`}
+								placement="bottom"
+							>
+								{open ? <ExpandLess /> : <ExpandMore />}
+							</Tooltip>
+							<Collapse in={open} timeout="auto" unmountOnExit>
+								<List className={classes.nested}>
+									{route.subRoutes.map((subRoute: RouteItem) => (
+										<MenuItem key={`${subRoute.key}`} route={subRoute} />
+									))}
+								</List>
+							</Collapse>
+						</ListItem>
+					) : (
+						<MenuItem key={route.key} route={route} />
+					)}
 				</>
 			))}
 		</List>
